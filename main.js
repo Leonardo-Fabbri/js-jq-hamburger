625ccb8df9b0baee9d97f8d1c4d0e3573afac152
Leonardo <leonardofabbri125@gmail.com>
2021-03-07 10:28:07 +0100

    definitivo

---
+++ main.js
@@ -0,0 +1,9 @@
+$( ".header-right a i" ).click(function() {
+    $( ".hamburger-menu" ).fadeIn();
+});
+
+
+$( ".hamburger-menu a i" ).click(function() {
+    $( ".hamburger-menu" ).fadeOut();
+});
+
